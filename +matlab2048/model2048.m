53696bfbafe14a140b64398fc290c8b7775471d8
Jean-Baptiste Lanfrey <jlanfrey@mathworks.com.au>
2014-04-22 16:47:00 +1000

    fixed error with randi called with empty index

--- +matlab2048/model2048.m
+++ +matlab2048/model2048.m
@@ -42,22 +42,23 @@ classdef model2048 < handle
 		end % main method
 		
 		function addNewBlock(obj)
-			if obj.isGameOver ==0
+			if obj.isGameOver == 0
 				% find the zeros and pick one randomly
 				zerosID = find(obj.Board==0);
-				num = randi(numel(zerosID),1);
-				
-				% add a 2 or a 4 (90% chance to add a 2, 10% chance to add a 4)
-				if randi(10) < 10
-					value = 2;
-				else
-					value = 4;
+				if ~isempty(zerosID)
+					num = randi(numel(zerosID),1);
+					
+					% add a 2 or a 4 (90% chance to add a 2, 10% chance to add a 4)
+					if randi(10) < 10
+						value = 2;
+					else
+						value = 4;
+					end
+					obj.Board(zerosID(num)) = value;
+					
+					% Broadcast event
+					notify(obj, 'BoardUpdated');
 				end
-				indexUpdated = zerosID(num);
-				obj.Board(indexUpdated) = value;
-				
-				% Broadcast event
-				notify(obj, 'BoardUpdated');
 				
 				% is the game over?
 				% if there is no zero and no identical contiguous numbers, the game is over
